c061bf02ad0ae413e648aabf8a355059d1726550
avisochek <avisochek3@gmail.com>
2016-02-25 13:25:50 -0500

    created clusters controller test

--- test/controllers/clusters_controller_test.rb
+++ test/controllers/clusters_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ClustersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "h1","Select a Request Type Below"
+  end
+  test "should get index" do
+    get :index , :request_type_id=>1
+    assert_response :success
+    assert_select "h3","Select a Cluster Below to View Details"
+    assert_select "h1",RequestType.find(1).name
+  end
+  test "should get show" do
+    cluster_id=1
+    get :show , :cluster_id=>cluster_id
+    assert_response :success
+    assert_select "h1",Cluster.find(cluster_id).request_type.name
+  end
 end
